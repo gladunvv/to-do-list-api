5ac99f7e62496da84b59c34c3a0d8bc4a146da59
Lancelot <bincha.1997@gmail.com>
2019-10-07 00:09:44 +0300

    update migrations

--- app/todolist/migrations/0007_auto_20191006_1911.py
+++ app/todolist/migrations/0007_auto_20191006_1911.py
@@ -4,7 +4,7 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
+    atomic = False
     dependencies = [
         ('todolist', '0006_auto_20191001_1739'),
     ]
